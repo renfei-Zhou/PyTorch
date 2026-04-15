7213205c1502a366aa220859c33b10e692e9f1ee
renfei-Zhou <tobyzrf1997@gmail.com>
2026-04-15 23:19:46 +0200

    learn batch, make plots

--- pytorch_tutorial/30_ComputerVision.py
+++ pytorch_tutorial/30_ComputerVision.py
@@ -38,6 +38,8 @@ test_data = datasets.FashionMNIST(
     transform=ToTensor(),
     target_transform=None
 )
+
+### 1.1 Check the proporties of the train_data
 # See the first training example
 image, label = train_data[0]
 # Check class names
@@ -56,10 +58,98 @@ print(f"Image label:\n{class_names[label]}\n")
     0->black, 1->full color [here is white], between 0 and 1 -> grey
 '''
 
+### 1.2 Visualing our data
+import matplotlib.pyplot as plt
+# image, label = train_data[0]
+# print(f"\nImage shape: {image.shape}")
+# plt.imshow(image.squeeze(), cmap="gray")
+# plt.title(class_names[label])
+# plt.axis(False)
+# plt.show()
+
+# # Plot more images
+# torch.manual_seed(42)
+# fig = plt.figure(figsize=(9,9))
+# rows, cols = 4, 4
+# for i in range(1, rows * cols + 1):
+#     random_idx = torch.randint(0, len(train_data), size=[1]).item()
+#     img, label = train_data[random_idx]
+#     fig.add_subplot(rows, cols, i)
+#     plt.imshow(img.squeeze(), cmap="gray")
+#     plt.title(class_names[label])
+#     plt.axis(False)
+# plt.show()
+
+'''
+    Question: 
+        Could the images be modelled with pure linear lines?
+        Or non-linearities need?
+'''
+
+
+### 2. Prepare DataLoader
+'''
+    Right now, our data is in the form of PyTorch Datasets.
+    DataLoader turns our dataset into a Python iterable.
+    More specifically, we want to turn our data into batches (or mini-batches).
+    
+    Why would we do this?
+    1. It is more computationally efficient, as in, your computing hardware 
+        may not be able to look (store in momery) at 60000 images in one hit.
+        So we break it down to 32 images at a time (batch size of 32).
+    2. It gives our neural network more chances to update its gradients per epoch.
+
+'''
+print(f"\n\ntrain_data:\n{train_data}\n\ntest_data:\n{test_data}")
+
+from torch.utils.data import DataLoader
+
+# Setup the batch size hyperparameter
+BATCH_SIZE = 32
+
+# Turn dataset into iterables (batches)
+train_dataloader = DataLoader(dataset=train_data,
+                              batch_size=BATCH_SIZE,
+                              shuffle=True)
+
+test_dataloader = DataLoader(dataset=test_data,
+                             batch_size=BATCH_SIZE,
+                             shuffle=True)
+
+# Let's check out what we've created
+print(f"\nDataLoaders: \n{train_dataloader}\n{test_dataloader}\n")
+print(f"Length of train_dataloader: {len(train_dataloader)} batches of {BATCH_SIZE}")
+print(f"Length of test_dataloader: {len(test_dataloader)} batches of {BATCH_SIZE}\n")
+
+# Check out what's inside the training dataloader
+train_features_batch, train_labels_batch = next(iter(train_dataloader))
+print(f"check first batch's shape: {train_features_batch.shape}, label shape: {train_labels_batch.shape}\n")
+
+# # Show a sample
+# torch.manual_seed(42)
+# random_idx = torch.randint(0, len(train_features_batch), size=[1]).item()
+# img, label = train_features_batch[random_idx], train_labels_batch[random_idx]
+# plt.imshow(img.squeeze(), cmap="gray")
+# plt.title(class_names[label])
+# plt.xlabel(f"Image size: {img.shape}, label: {label}, label size: {label.shape}")
+# plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 debug=1
 # 13_48_09 (PyTorch for Deep Learning & Machine Learning – Full Course)
 # 14_46_03
-# 14_51_41 too busy on work and driving
+# 14_51_41
+# 15_21_15 (2026-04-15)
